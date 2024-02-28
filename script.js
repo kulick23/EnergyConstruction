39a5a16edd05ca031c8aac9e6e31dba7380daaa5
Danila Kulinkovich <danek200326@gmail.com>
2024-02-28 15:56:12 +0200

    firsr commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider .slider-line');
+const sliderNextBtn = document.querySelector('.slider-next');
+const sliderPrevBtn = document.querySelector('.slider-prev');
+const bars = document.querySelectorAll('.bar');
+let count = 0;
+let width;
+
+function init() {
+    width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+    adjustButtonsPosition();
+}
+
+init();
+window.addEventListener('resize', function() {
+    init();
+});
+
+sliderNextBtn.addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+    updateBars();
+});
+
+sliderPrevBtn.addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+    updateBars();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
+
+function updateBars() {
+    bars.forEach(bar => bar.classList.remove('active-bar')); 
+    bars[count].classList.add('active-bar'); 
+}
+
+function adjustButtonsPosition() {
+    const sliderHeight = document.querySelector('.slider').offsetHeight;
+    const buttonHeight = sliderNextBtn.offsetHeight;
+    const topOffset = (sliderHeight - buttonHeight) / 1.8;
+
+    sliderNextBtn.style.top = topOffset + 'px';
+    sliderPrevBtn.style.top = topOffset + 'px';
+}
